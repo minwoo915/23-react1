3710c54df3453415624b83481ee42d90855f533f
minwoo <minwoo915@naver.com>
2023-03-30 10:30:00 +0900

    Add chapter_03
    오류 수정2

--- src/chapter_03/Library.jsx
+++ src/chapter_03/Library.jsx
@@ -4,9 +4,9 @@ import Book from "./Book"
 function Library(props) {
     return (
         <div>
-            <Book name="처음 만난 파이썬" numOfpage={300} />
-            <Book name="처음 만난 AWS" numOfpage={400} />
-            <Book name="처음 만난 리액트" numOfpage={500} />
+            <Book name="처음 만난 파이썬" numOfPage={300} />
+            <Book name="처음 만난 AWS" numOfPage={400} />
+            <Book name="처음 만난 리액트" numOfPage={500} />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(
   <React.StrictMode>
     <Library />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
